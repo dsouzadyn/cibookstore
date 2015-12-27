28b2cf8253ed7d68bd28bdb64076b400eb284b3b
dsouzadyn <dsouzadyn@gmail.com>
2015-12-28 00:23:46 +0530

    Navbar for dashboard added

---
+++ application/views/templates/dashboard_navbar.php
@@ -0,0 +1,5 @@
+<nav>
+    <li class="brand"><a href="#">BookStore</a></li>
+    <li><a href='<?php echo base_url().'new';?>'>New</a></li>
+    <li><a href=<?php echo base_url().'logout'?>>Logout</a></li>
+</nav>
